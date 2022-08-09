61d0b6334404b4573d650fa7c186929218157909
leonsteinhaeuser <leonsteinhaeuser@gmx.de>
2022-08-09 10:17:34 +0200

    feature: implemented postfix docker image build

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM alpine:latest
+
+LABEL maintainer="DevOps <devops@fylr.io>"
+
+COPY entrypoint.sh /entrypoint.sh
+
+RUN apk add --no-cache --update postfix cyrus-sasl ca-certificates bash && \
+    apk add --no-cache --upgrade musl musl-utils && \
+    rm -rf /var/cache/apk/* && \
+    sed -i -e 's/inet_interfaces = localhost/inet_interfaces = all/g' /etc/postfix/main.cf
+
+# delete old pid file
+RUN rm -f /var/spool/postfix/pid/master.pid && mkdir -p /postfix/sasl
+
+# mark used directories
+VOLUME [ "/var/spool/postfix", "/etc/postfix" ]
+
+EXPOSE 25
+
+ENTRYPOINT [ "/entrypoint.sh" ]
+
+CMD [ "postfix", "start-fg" ]
---
+++ entrypoint.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "extracting env..."
+envVarPrefix=POSTFIX_CFG_
+
+envVars=$(env | grep $envVarPrefix)
+
+mkdir -p /var/spool/postfix/
+mkdir -p /var/spool/postfix/pid
+
+# split env key=value combination in key and value
+# apply key and value with postconf
+for entry in $envVars; do
+    value=${entry#*=}
+    key=${entry%"$value"}
+    second=$(echo "$key" | sed -e "s+=++g" -e "s+$envVarPrefix++g")
+
+    # we extracted the env vars, let's apply them
+    echo -e "executing command: [ postconf \"${second,,} = $value\" || exit 1 ]"
+    postconf -e "${second,,} = ${value}" || exit 1
+done
+
+postmap /postfix/sasl/sasl_passwd
+cp /postfix/sasl/sasl_passwd.lmdb /etc/postfix/sasl/
+cp /postfix/sasl/sasl_passwd /etc/postfix/sasl_passwd
+
+echo "starting postfix with command: $@..."
+exec "$@"
